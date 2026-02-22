8df9dcccc9c81c272329d7c59fd11d42295c4df7
RafhyKhan <Rafhy.mk@hotmail.com>
2026-02-22 02:40:55 -0700

    Updating routes
    Adding JSON format input

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Item(BaseModel):
+  text: str = None
+  is_done: bool = False
 
 items = []
 
@@ -13,22 +17,22 @@ def root():
 
 
 @app.post("/items")
-def create_item(item: str):
+def create_item(item: Item):
   items.append(item)
   return items
 
 
-@app.get("/items")
+@app.get("/items", response_model=list[Item])
 def list_items(limit: int = 10):
   return items[0:limit]
 
 
-@app.get("/items/{item_id}")
-def get_item(item_id: int) -> str:
+@app.get("/items/{item_id}", response_model=Item)
+def get_item(item_id: int) -> Item:
   if item_id < len(items): 
     return items[item_id]
   else:
-    raise HTTPException(status_code=404, detail="Item not found")
+    raise HTTPException(status_code=404, detail=f"Item {item_id} not found")
 
 
 
